a450b7d3ddba9f8783f2148f26ba93eea85adaba
aaronwayas <avazquezwayas@gmail.com>
2024-06-23 16:29:53 -0600

    add: Route System

--- main.py
+++ main.py
@@ -1,50 +1,40 @@
-import json
 import flet as ft
 
 link_discord = "https://discord.gg/vcQMwRzbak"
 
 news = [
-    (
-        """
-Version 1.0.0,
-Add: News system
-https://discord.gg/vcQMwRzbak 🤓
-""",
-        "11/06/2022",
-    ),
-    ("Oasis 1.0.0.0", "12/06/2022"),
-    (
-        """
-Version 1.0.0,
-Add: News system
-https://discord.gg/vcQMwRzbak 🤓
-""",
-        "11/06/2022",
-    ),
-    ("Oasis 1.0.0.0", "12/06/2022"),
-    (
-        """
-Version 1.0.0,
-Add: News system
-https://discord.gg/vcQMwRzbak 🤓
-""",
-        "11/06/2022",
-    ),
-    ("Oasis 1.0.0.0", "12/06/2022"),
+    ("Version 1.0.0, Add: News system", "11/06/2022"),
+    ("Version 1.0.0, Add: News system", "11/06/2022"),
+    ("Version 1.0.0, Add: News system", "11/06/2022"),
+    ("Version 1.0.0, Add: News system", "11/06/2022"),
+    ("Version 1.0.0, Add: News system", "11/06/2022"),
+    ("Version 1.0.0, Add: News system", "11/06/2022"),
+    ("Version 1.0.0, Add: News system", "11/06/2022"),
+    ("Version 1.0.0, Add: News system", "11/06/2022"),
+    ("Version 1.0.0, Add: News system", "11/06/2022"),
+    ("Version 1.0.0, Add: News system", "11/06/2022"),
+    ("Version 1.0.0, Add: News system", "11/06/2022"),
+    ("Version 1.0.0, Add: News system", "11/06/2022"),
+    ("Version 1.0.0, Add: News system", "11/06/2022"),
+    ("Version 1.0.0, Add: News system", "11/06/2022"),
+    ("Version 1.0.0, Add: News system", "11/06/2022"),
+    ("Version 1.0.0, Add: News system", "11/06/2022"),
+    ("Version 1.0.0, Add: News system", "11/06/2022"),
+    ("Version 1.0.0, Add: News system", "11/06/2022"),
 ]
 
-
-def open_url(url):
-    def _open_url(e):
-        e.page.launch_url(url)
-
-    return _open_url
+updates = [
+    ("Version 1.1.0, Update: Bug fixes", "12/07/2022"),
+    ("Version 1.1.0, Update: Performance improvements", "12/07/2022"),
+    ("Version 1.1.0, Update: New feature added", "12/07/2022"),
+    ("Version 1.1.0, Update: UI improvements", "12/07/2022"),
+]
 
 
 def create_new(text, fecha):
     return ft.Container(
         content=ft.Column(
-            [ft.Text(fecha, size=12), ft.Text(text)],
+            [ft.Text(fecha, size=12, color="black"), ft.Text(text, color="black")],
         ),
         bgcolor="#9eaab6",
         border_radius=12,
@@ -53,21 +43,28 @@ def create_new(text, fecha):
     )
 
 
-def load_config(file):
-    with open(file, "r") as i:
-        config = json.load(i)
-    return config
-
-
 def main(page: ft.Page):
     page.title = "Oasis Client"  # Nombre del cliente
     page.window_always_on_top = True
     page.bgcolor = "#1A1C1E"
 
-    # Botón de jugar
-    play_button = ft.FilledButton("JUGAR")
+    version_button = ft.FilledButton(
+        content=ft.Text("VERSION", color="white", size=17),
+        style=ft.ButtonStyle(
+            bgcolor="#607885", shape=ft.RoundedRectangleBorder(radius=10)
+        ),
+        width=150,
+        height=45,
+    )
+    play_button = ft.FilledButton(
+        content=ft.Text("JUGAR", color="white", size=17),
+        style=ft.ButtonStyle(
+            bgcolor="#59C4FF", shape=ft.RoundedRectangleBorder(radius=15)
+        ),
+        width=150,
+        height=45,
+    )
 
-    # Imagen principal
     image = ft.Container(
         content=ft.Image("assets/image.png", fit=ft.ImageFit.SCALE_DOWN),
         height=500,
@@ -77,66 +74,187 @@ def main(page: ft.Page):
         padding=ft.padding.only(left=60),
     )
 
-    # Botones de configuración, Discord y Home
-    settings_button = ft.Container(
-        content=ft.IconButton(
-            ft.icons.SETTINGS, icon_color="white", width=50, height=50
+    news_container = ft.Container(
+        content=ft.Column(
+            [create_new(x[0], x[1]) for x in news],
+            spacing=10,
+            alignment=ft.MainAxisAlignment.CENTER,
+            scroll=ft.ScrollMode.AUTO,  # Enable scrolling
+            height=400,  # Adjust the height as needed
         ),
-        alignment=ft.alignment.top_center,
+        alignment=ft.alignment.center,
+        height=400,  # Adjust the height as needed
     )
 
-    discord_button = ft.Container(
-        content=ft.IconButton(
-            ft.icons.DISCORD,
-            icon_color="white",
-            width=50,
-            height=50,
-            on_click=open_url(link_discord),
-        ),
-        alignment=ft.alignment.top_center,
+    news_button_style = ft.ButtonStyle(
+        bgcolor=ft.colors.TRANSPARENT,
+        shape=ft.RoundedRectangleBorder(radius=10),
     )
 
-    home_button = ft.Container(
-        content=ft.IconButton(ft.icons.HOME, icon_color="white", width=50, height=50),
-        alignment=ft.alignment.top_center,
+    updates_button_style = ft.ButtonStyle(
+        bgcolor=ft.colors.TRANSPARENT,
+        shape=ft.RoundedRectangleBorder(radius=10),
     )
 
-    # Añadiendo todos los componentes a la página
-    page.add(
-        ft.Column(
+    news_button = ft.TextButton(
+        "NEW's",
+        style=news_button_style,
+        on_click=lambda e: show_news(e, news_button, updates_button),
+    )
+    updates_button = ft.TextButton(
+        "UPDATE's",
+        style=updates_button_style,
+        on_click=lambda e: show_updates(e, news_button, updates_button),
+    )
+
+    def show_news(e, news_btn, updates_btn):
+        news_container.content.controls = [create_new(x[0], x[1]) for x in news]
+        news_btn.style.bgcolor = "#242a30"
+        updates_btn.style.bgcolor = ft.colors.TRANSPARENT
+        page.update()
+
+    def show_updates(e, news_btn, updates_btn):
+        news_container.content.controls = [create_new(x[0], x[1]) for x in updates]
+        news_btn.style.bgcolor = ft.colors.TRANSPARENT
+        updates_btn.style.bgcolor = "#242a30"
+        page.update()
+
+    bar_container = ft.Container(
+        content=ft.Row(
             [
+                ft.IconButton(
+                    ft.icons.SETTINGS,
+                    icon_color="white",
+                    width=50,
+                    height=50,
+                    on_click=lambda e: page.go("/settings"),
+                ),
+                image,
+                ft.IconButton(
+                    ft.icons.DISCORD,
+                    icon_color="white",
+                    width=50,
+                    height=50,
+                    url=link_discord,
+                ),
+                ft.IconButton(
+                    ft.icons.HOME,
+                    icon_color="white",
+                    width=50,
+                    height=50,
+                    on_click=lambda e: page.go("/"),
+                ),
+            ],
+            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+            vertical_alignment=ft.CrossAxisAlignment.START,
+        ),
+        height=200,
+    )
+
+    main_container = ft.Container(
+        content=ft.Column(
+            [
+                bar_container,
                 ft.Container(
-                    ft.Row(
-                        [settings_button, image, discord_button, home_button],
-                        alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
-                        vertical_alignment=ft.CrossAxisAlignment.START,
+                    content=ft.Column(
+                        [
+                            ft.Container(
+                                content=ft.Row(
+                                    [
+                                        news_button,
+                                        updates_button,
+                                    ],
+                                    alignment=ft.MainAxisAlignment.CENTER,
+                                ),
+                            ),
+                            news_container,
+                        ],
+                        alignment=ft.MainAxisAlignment.CENTER,
+                        height=400,
                     ),
-                    height=210,
+                    expand=True,
+                    alignment=ft.alignment.center,
+                    padding=ft.padding.all(10),
                 ),
                 ft.Container(
                     content=ft.Row(
-                        [ft.TextButton("NEW's"), ft.TextButton("UPDATE's")],
-                        alignment=ft.MainAxisAlignment.CENTER,
+                        [
+                            version_button,
+                            play_button,
+                        ],
+                        alignment=ft.MainAxisAlignment.END,
                     ),
-                    alignment=ft.alignment.center,
-                ),
-                ft.Container(
-                    content=ft.Column(
-                        [create_new(i, e) for i, e in news],
-                        scroll=ft.ScrollMode.HIDDEN,
-                    ),
-                    bgcolor="red",
-                    padding=ft.padding.all(27),
-                    alignment=ft.alignment.center,
-                    height=400,
-                ),
-                ft.Container(
-                    content=play_button,
+                    height=55,
+                    bgcolor="#141618",
+                    padding=ft.padding.only(right=10, left=10),
+                    border_radius=ft.border_radius.all(20),
                 ),
             ],
-            spacing=10,
-        )
+            spacing=1,
+        ),
+        padding=ft.padding.all(1),
+        expand=True,
+        alignment=ft.alignment.center,
     )
+
+    def route_change(route):
+        if page.route == "/":
+            page.views.clear()
+            page.views.append(
+                ft.View(
+                    "/",
+                    [main_container],
+                )
+            )
+        elif page.route == "/download":
+            page.views.append(
+                ft.View(
+                    "/download",
+                    [],
+                )
+            )
+
+        elif page.route == "/settings":
+            page.views.append(
+                ft.View(
+                    "/settings",
+                    [bar_container],
+                )
+            )
+
+        else:
+            page.views.append(
+                ft.View(
+                    "/404",
+                    [
+                        ft.Container(
+                            content=ft.Column(
+                                [
+                                    ft.Text("404", size=40),
+                                    ft.Text("Page not found", size=20),
+                                    ft.ElevatedButton(
+                                        "Go Home", on_click=lambda _: page.go("/")
+                                    ),
+                                ],
+                                alignment=ft.MainAxisAlignment.CENTER,
+                            ),
+                            expand=True,
+                            alignment=ft.alignment.center,
+                        )
+                    ],
+                )
+            )
+
+        page.update()
+
+    def view_pop(view):
+        page.views.pop()
+        top_view = page.views[-1]
+        page.go(top_view.route)
+
+    page.on_route_change = route_change
+    page.on_view_pop = view_pop
+    page.go("/")
 
 
 ft.app(target=main)
